fe5e5ab86cf2fe90a2ccf7f2a32404dc80ce785c
zackatcorpa <zack@corpa.io>
2015-08-13 14:50:19 -0700

    Initial Commit

---
+++ DemoAC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoAC
+//
+//  Created by Zack Zhu on 8/13/15.
+//  Copyright (c) 2015 Zack Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
